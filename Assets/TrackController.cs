1c23b1d43111a5292fc8c02d18e6ebd511294797
Richard Sivak <sivak.r101@gmail.com>
2016-10-23 01:28:34 -0400

    Initial Commit

---
+++ Assets/TrackController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets
+{
+
+    public class TrackController : MonoBehaviour
+    {
+        private PSMoveAPI api;
+        private PSMoveController controller;
+        private PSMoveDataContext dataContext;
+        private PSMoveHitchWatchdog clock;
+        private PSMoveManager manager;
+        private PSMovePose pose;
+        private PSMoveTest test;
+        private PSMoveUtility utility;
+
+        // Use this for initialization
+        void Start()
+        {
+            api = new PSMoveAPI();
+            controller = new PSMoveController();
+            manager = new PSMoveManager();
+            pose = new PSMovePose();
+            test = new PSMoveTest();
+            utility = new PSMoveUtility();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
